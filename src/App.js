bed93a5ea1e9a3ff1a49cbb78ee72550f0d46e27
owenliversidge <owen.liversidge@gmail.com>
2019-02-08 16:49:55 -0500

    added prop passdowns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to the home page</h1>
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="john@gmail.com" phone="555-555-5555" />
+        <Contact name="Jane Doe" email="jane@gmail.com" phone="333-333-3333" />
       </div>
     );
   }
