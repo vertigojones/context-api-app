0e24edc8d39e4c4055d12ea92ad6fd0358ab4588
owenliversidge <owen.liversidge@gmail.com>
2019-02-08 17:32:51 -0500

    added bootstrap and restructured state management

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 
 class Contact extends Component {
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
 
     return (
-      <div>
+      <div className="card card-body mb-3">
         <h4>{name}</h4>
-        <ul>
-          <li>Email: {email}</li>
-          <li>Phone: {phone}</li>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
@@ -18,9 +18,7 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
---
+++ src/components/Contacts.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Owen Liversidge",
+        email: "owen@gmail.com",
+        phone: "111-111-1111"
+      },
+      {
+        id: 2,
+        name: "Sweety James",
+        email: "sweety@gmail.com",
+        phone: "222-222-2222"
+      },
+      {
+        id: 3,
+        name: "Ruby Liversidge",
+        email: "ruby@gmail.com",
+        phone: "333-333-3333"
+      }
+    ]
+  };
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Contacts;
